03f16872aa1643d62f6404e9194566f3e5475a1a
Chase Hayes <188492039+chasejhayes@users.noreply.github.com>
2026-06-15 13:48:59 -0400

    Add basic post request

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,30 @@ const app = express();
 
 app.use(express.json())
 
-app.get('/api/media', (request, response) => {
-    response.json([{test: "test"}])
+let media = [
+    {title: "Perfume", dateFinished: "4/5/26", rating: "5/5 stars"},
+    {title: "Body Double", dateFinished: "6/4/26", rating: "3.5/5 stars"},
+    {title: "Malazan Book 2", dateFinished: "5/22/26", rating: "3.5/5 stars" }
+]
+
+app.get('/api/media/', (request, response) => {
+    response.json(media)
+})
+
+app.post('/api/media', (request, response) => {
+    let mediaData = request.body;
+
+    let newMedia = {
+        "title": mediaData.title,
+        "dateFinished": mediaData.dateFinished,
+        "rating": mediaData.rating
+    }
+
+    media = media.concat(newMedia)
+
+    response.status(201).json(mediaData)
+
+
 })
 
 const PORT = 3001
@@ -12,3 +34,14 @@ const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}.`)
 })
+
+
+/*
+test
+{
+        "title": "Russian Course",
+        "dateFinished": "Ongoing",
+        "rating": "Incomplete"
+    }
+
+*/
