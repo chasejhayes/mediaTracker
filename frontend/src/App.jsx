81c9dbae76dcb65b9ef2b6ff52dffba9d6452f7c
Chase Hayes <188492039+chasejhayes@users.noreply.github.com>
2026-07-22 12:33:47 -0400

    Move gitignore to root. Add .env to gitignore

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,130 +1,14 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import './services/axios.js'
 import './style.css'
-
-const Header = ({ text, id }) => {
-  return (
-    <div id={id}>
-      <h1>{text}</h1>
-    </div>
-  )
-}
-
-
-const Button = ({ onClick, text }) => {
-  return (
-    <button onClick={onClick}>{text}</button>
-  )
-}
-
-
-const SortMenu = ({ options, selectedValue, onChange, text, htmlFor }) => (
-  <div>
-    <label htmlFor={htmlFor}>{text}</label>
-    <select name="media" id="media" value={selectedValue} onChange={onChange}>
-      <option value="">Select an Options...</option>
-      {options.map((option) => (
-        <option key={option.value} value={option.value}>
-          {option.label}
-        </option>
-      ))}
-    </select>
-  </div>
-)
-
-function UserForm({
-  onSubmit,
-  newTitle,
-  setNewTitle,
-  newRating,
-  setNewRating,
-  newFinishDate,
-  setNewFinishDate,
-  showForm
-  }) {
-  
-  if(showForm === true)
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <label>Title:
-          <input
-            value={newTitle}
-            onChange={(e) => setNewTitle(e.target.value)} />
-        </label>
-        <label>Rating:
-          <input
-            value={newRating}
-            onChange={(e) => setNewRating(e.target.value)} />
-        </label>
-        <label>Finish Date:
-          <input
-            value={newFinishDate}
-            onChange={(e) => setNewFinishDate(e.target.value)} />
-        </label>
-        <button type="submit">add</button>
-      </form>
-    </div>
-  )
-}
-
-function SearchBar({ test, handleSearchChange }) {
-  return (
-    <label htmlFor="">Search:
-      <input value={test} onChange={(e) => handleSearchChange(e)} /></label>
-  )
-}
-
-
-function FilterMenu({ onChange }){
-
-  return (
-     <div>
-      <label htmlFor="media">Filter by Rating</label>
-      <select name="media" id="media" onChange={onChange}>
-        <option value="default">All</option>
-        <option value="one">1 Star</option>
-        <option value="two">2 Star</option>
-        <option value="three">3 Star</option>
-        <option value="four">4 Star</option>
-        <option value="five">5 Star</option>
-      </select>
-    </div>
-  )
-
-}
-
-function MediaDisplay(){
-    let displayType = media;
-    if (toggleFilter) {
-      displayType = filter
-    } else if (toggleSearch) {
-      displayType = searchArr
-    }
-
-    return (
-      <div>
-        <ul id='card'>
-          {{displayType}.map((media) =>
-            <div className='list_item'>
-              <li key={media.id}>
-                <h2>{media.title}</h2>
-                <p>Date Finished: {media.dateFinished}</p>
-                <p>Rating: {media.rating}</p>
-                <button onClick={() => deleteMedia(media.id)}
-                >Delete</button>
-                <button onClick={() => { setNewId(media.id); handleShowEditForm() }}>
-                  Edit
-                </button>
-              </li>
-            </div>
-          )}
-        </ul>
-      </div>
-    )
-
-}
-
+import Header from './components/Header.jsx'
+import Button from './components/Button.jsx'
+import SearchBar from './components/SearchBar.jsx'
+import FilterMenu from './components/FilterMenu.jsx'
+import SortMenu from './components/SortMenu.jsx'
+import UserForm from './components/UserForm.jsx'
+import MediaDisplay from './components/MediaDisplay.jsx'
 
 
 function App() {
@@ -134,15 +18,13 @@ function App() {
   const [newFinishDate, setNewFinishDate] = useState('')
   const [showForm, setShowForm] = useState(false)
   const [sort, setSort] = useState('default')
-  const [filter, setFilter] = useState('default')
-  const [toggleFilter, setToggleFilter] = useState(false)
   const [showEditForm, setShowEditForm] = useState(false)
   const [newId, setNewId] = useState("")
   const [test, setTest] = useState("")
   const [searchArr, setSearchArr] = useState([])
   const [toggleSearch, setToggleSearch] = useState(false)
-
-  const [displayType, setDisplayType] = useState("media")
+  const [filter, setFilter] = useState('default')
+  const [toggleFilter, setToggleFilter] = useState(false)
 
 
 
@@ -152,7 +34,6 @@ function App() {
         setMedia(response.data)
       })
   }, [])
-
 
   function addMedia(e) {
     e.preventDefault()
@@ -182,7 +63,7 @@ function App() {
       })
   }
 
-  function editRating(e) {
+  function editMedia(e) {
     e.preventDefault()
     let id = newId;
     console.log(id)
@@ -205,165 +86,30 @@ function App() {
       .catch(error => {
         console.log(error)
       })
-      setShowEditForm(false)
+    setShowEditForm(false)
   }
 
-
-  const sortOptions = [
-    { value: "al", label: "Alphabetical" },
-    { value: "rating", label: "Rating" },
-    { value: "finished", label: "Finished" }
-  ]
-
-
-  function handleShowForm() {
-    if (showForm == false) {
-      setShowForm(true);
-    } else {
-      setShowForm(false)
-    }
-  }
-
-  function handleShowEditForm() {
-    if (showEditForm == false) {
-      setShowEditForm(true)
-    } else {
-      setShowEditForm(false)
-    }
-  }
-
-  const mediaDisplay = () => {
-    let displayType = media;
-    if (toggleFilter) {
-      displayType = filter
-    } else if (toggleSearch) {
-      displayType = searchArr
-    }
-
-    return (
-      <div>
-        <ul id='card'>
-          {displayType.map((media) =>
-            <div className='list_item'>
-              <li key={media.id}>
-                <h2>{media.title}</h2>
-                <p>Date Finished: {media.dateFinished}</p>
-                <p>Rating: {media.rating}</p>
-                <button onClick={() => deleteMedia(media.id)}
-                >Delete</button>
-                <button onClick={() => { setNewId(media.id); handleShowEditForm() }}>
-                  Edit
-                </button>
-              </li>
-            </div>
-          )}
-        </ul>
-      </div>
-    )
-  }
-
-
-
-
-
-  const handleFilterChange = (e) => {
-    const selectedValue = e.target.value;
-    if (selectedValue === 'default') {
-      setToggleFilter(false)
-    } else if (selectedValue === "one") {
-      setToggleFilter(true)
-      return setFilter(media.filter(media => media.rating === 1))
-    }
-    else if (selectedValue === "two") {
-      setToggleFilter(true)
-      return setFilter(media.filter(media => media.rating === 2))
-    } else if (selectedValue === "three") {
-      setToggleFilter(true)
-      return setFilter(media.filter(media => media.rating === 3))
-    }
-    else if (selectedValue === "four") {
-      setToggleFilter(true)
-      return setFilter(media.filter(media => media.rating === 4))
-    } else if (selectedValue === "five") {
-      setToggleFilter(true)
-      return setFilter(media.filter(media => media.rating === 5))
-    }
-  }
-
-    const handleSortChange = (e) => {
-    const selectedValue = e.target.value;
-    setSort(selectedValue)
-    if (selectedValue === 'default') {
-      console.log(media)
-    } else if (selectedValue === 'al') {
-      sortByTitle()
-      console.log(media)
-    } else if (selectedValue === 'rating') {
-      sortByRating()
-    } else if (selectedValue === 'finished') {
-      sortByDate()
-      console.log(media)
-    }
-  }
-
-
-
-  function sortByRating() {
-    return setMedia(
-      media.sort((a, b) => b.rating - a.rating)
-    )
-  }
-
-  function sortByTitle() {
-    return setMedia(
-      media.toSorted((a, b) => a.title.localeCompare(b.title))
-    )
-  }
-
-  function sortByDate() {
-    return setMedia(
-      media.sort((a, b) => new Date(b.dateFinished) - new Date(a.dateFinished))
-    )
-  }
-
-
-
-
-  const handleSearchChange = (e) => {
-    setToggleSearch(true)
-    setTest(e.target.value)
-    let filtered = media.filter(media => media.title.startsWith(e.target.value))
-    setSearchArr(filtered)
-    if (e.target.value === "") {
-      setToggleSearch(false)
-    }
-  }
 
 
 
   return (
     <div>
-      <Header text="My media" id="header_div" />
-      <Button text="Add Media" onClick={handleShowForm} />
-      <SortMenu text="Sort By:" htmlFor="media" onChange=
-        {handleSortChange} value={sort} options={sortOptions} />
+      <Header text="Media Tracker" id="header_div" />
+      <Button text="Add Media" setShowForm={setShowForm} showForm={showForm} />
+      <SortMenu text="Sort By:" htmlFor="media" media={media} value={sort} setSort={setSort} setMedia={setMedia}/>
 
       <UserForm onSubmit={addMedia} newTitle={newTitle} setNewTitle={setNewTitle} newRating={newRating} setNewRating={setNewRating} newFinishDate={newFinishDate} setNewFinishDate={setNewFinishDate} showForm={showForm} />
 
-      <UserForm onSubmit={editRating} newTitle={newTitle} setNewTitle={setNewTitle} newRating={newRating} setNewRating={setNewRating} newFinishDate={newFinishDate} setNewFinishDate={setNewFinishDate} showForm={showEditForm} />
+      <UserForm onSubmit={editMedia} newTitle={newTitle} setNewTitle={setNewTitle} newRating={newRating} setNewRating={setNewRating} newFinishDate={newFinishDate} setNewFinishDate={setNewFinishDate} showForm={showEditForm} />
 
-      <SearchBar value={test} handleSearchChange={handleSearchChange} />
+      <SearchBar value={test} setToggleSearch={setToggleSearch} setTest={setTest} setSearchArr={setSearchArr} media={media} />
 
-      <FilterMenu onChange={handleFilterChange}/>
-      {/* {showForm && addMediaForm()} */}
-      {mediaDisplay()}
-      {/* {showEditForm && editForm()} */}
+      <FilterMenu media={media} filter={filter} setFilter={setFilter} toggleFilter={toggleFilter} setToggleFilter={setToggleFilter} />
 
+      <MediaDisplay media={media} toggleFilter={toggleFilter} filter={filter} toggleSearch={toggleSearch} searchArr={searchArr} deleteMedia={deleteMedia} setNewId={setNewId} showEditForm={showEditForm} setShowEditForm={setShowEditForm} />
 
     </div>
   )
 }
 
 export default App
-
-
